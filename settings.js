3992b47f0652154bfc3dc09385b8722d9d0d9114
Mahiruamanda20 <151424642+Mahiruamanda20@users.noreply.github.com>
2025-06-29 19:31:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=h5YGiKhC#y-KawrbCgwuDcDxp9nC4rN2g1TWRIg-W-b_DDVAciRI",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
